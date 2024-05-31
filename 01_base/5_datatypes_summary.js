3619121d68fd13f892724c41268af48c4e15919b
ANKIT JANGID <32219733+AnkitJangir1997@users.noreply.github.com>
2024-05-31 17:30:07 +0000

    Opreations

---
+++ 01_base/5_datatypes_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+//  7 types : string, number, boolearn, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34567894561n
+
+
+
+
+//  Reference ( Non primitive )
+
+//  Array, Objects, Funnction
+
+const heros = [ "hero 1", "hero 2", "hero 3" ]
+let myobj = {
+    name: "Ankit",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello Word");
+}
+
+console.log(typeof score);
