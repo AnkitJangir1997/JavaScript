c88a6e87da8076713dc361c65f9b9eef4659bfbb
ANKIT JANGID <32219733+AnkitJangir1997@users.noreply.github.com>
2024-06-02 17:15:34 +0000

    Numbers

---
+++ 01_base/6_num_and_math.js
@@ -0,0 +1,23 @@
+console.log("Hello");
+
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const other = 135.55
+
+console.log(other.toPrecision(3));
+const other1 = 100000
+
+console.log(other1.toLocaleString('en-IN'));
+console.log(other1.toLocaleString());
+
+
+
+// +++++++++++++    MAthes   ++++++++++
+
