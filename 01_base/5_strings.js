24334373225f3aa387b84c8db69cce64a4768d09
ANKIT JANGID <32219733+AnkitJangir1997@users.noreply.github.com>
2024-06-02 17:15:24 +0000

    String

---
+++ 01_base/5_strings.js
@@ -0,0 +1,40 @@
+const  name = "Ankit-hello"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('Ankit-j')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+console.log(gameName.charAt('2'));
+console.log(gameName.indexOf('10'));
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const antherString = gameName.slice(-5, 4)
+console.log(antherString);
+
+
+const newStringOne = "  ank    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+console.log(newStringOne.trimEnd());
+
+
+const url = "https://ankitjangid.com"
+
+
+
+
+
+console.log(gameName.split('-'));
+
