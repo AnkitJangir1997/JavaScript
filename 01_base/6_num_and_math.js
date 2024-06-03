fca7348457ae836c6a8d42abae893bab55d758d3
Ankit Jangid <ankitjangir1997@gmail.com>
2024-06-03 22:19:49 +0530

    Number  and maths learning

--- 01_base/6_num_and_math.js
+++ 01_base/6_num_and_math.js
@@ -1,23 +1,36 @@
-console.log("Hello");
+// console.log("Hello");
 
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
 const other = 135.55
 
-console.log(other.toPrecision(3));
+// console.log(other.toPrecision(3));
 const other1 = 100000
 
-console.log(other1.toLocaleString('en-IN'));
-console.log(other1.toLocaleString());
+// console.log(other1.toLocaleString('en-IN'));
+// console.log(other1.toLocaleString());
 
 
 
 // +++++++++++++    MAthes   ++++++++++
 
+
+console.log(Math);
+
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+Math.random()
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)         // Important line
